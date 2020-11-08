89ee403e0acd570fb5beb3c9ca4c77569208ad38
Max Bregman <bregmanmax91@gmail.com>
2020-11-08 16:22:55 -0700

    can now get a win with any row

--- game.js
+++ game.js
@@ -16,9 +16,17 @@ class Game {
         this.playerTwo.turn = !this.playerTwo.turn;
     }
     checkFoWin(player) {
-        if (this.gameBoard[0] === this.gameBoard[1] && this.gameBoard[0] === this.gameBoard[2] && this.gameBoard[1] === player.token) {
+        if (this.checkRows()) {
             console.log(this.gameBoard[0], this.gameBoard[1], this.gameBoard[2]);
             player.winCount++;
         }
+    }
+    checkRows() {
+        if ((this.gameBoard[0] === this.gameBoard[1] && this.gameBoard[0] === this.gameBoard[2] && this.gameBoard[2] !== '') || (this.gameBoard[3] === this.gameBoard[4] && this.gameBoard[3] === this.gameBoard[5] && this.gameBoard[5] !== '') || (this.gameBoard[6] === this.gameBoard[7] && this.gameBoard[6] === this.gameBoard[8] && this.gameBoard[8] !== '')) {
+            return true;
+
+        } else {
+            return false;
+        };
     }
 }
