8a272ff6eaf94a686933b2694394db4689a217c4
Max Bregman <bregmanmax91@gmail.com>
2020-11-08 15:41:02 -0700

    can now win a game with a top row strikeout, also now accounts for slots on the gameboard that have already been filled and makes sure the turn boolean stays correct when trying to play an already filled slot, and a win is added to winning players add count

--- game.js
+++ game.js
@@ -5,13 +5,20 @@ class Game {
         this.gameBoard = ['', '', '', '', '', '', '', '', ''];
     }
     playTurn(player, tilePlayed) {
-        if (player.turn === true) {
+        if (player.turn === true && tilePlayed < 9 && this.gameBoard[tilePlayed] === '') {
             this.gameBoard.splice(tilePlayed, 1, player.token);
             this.switchTurns();
+            this.checkFoWin(player);
         }
     }
     switchTurns() {
         this.playerOne.turn = !this.playerOne.turn;
         this.playerTwo.turn = !this.playerTwo.turn;
+    }
+    checkFoWin(player) {
+        if (this.gameBoard[0] === this.gameBoard[1] && this.gameBoard[0] === this.gameBoard[2] && this.gameBoard[1] === player.token) {
+            console.log(this.gameBoard[0], this.gameBoard[1], this.gameBoard[2]);
+            player.winCount++;
+        }
     }
 }
