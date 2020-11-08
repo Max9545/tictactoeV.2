0685fb7c885a695d9b4a624d05908d142c956d18
Max Bregman <bregmanmax91@gmail.com>
2020-11-08 16:48:15 -0700

    can now win with diagonals

--- game.js
+++ game.js
@@ -16,7 +16,7 @@ class Game {
         this.playerTwo.turn = !this.playerTwo.turn;
     }
     checkFoWin(player) {
-        if (this.checkRows() || this.checkColumns()) {
+        if (this.checkRows() || this.checkColumns() || this.checkDiagonals()) {
             console.log(this.gameBoard[0], this.gameBoard[1], this.gameBoard[2]);
             player.winCount++;
         }
@@ -31,6 +31,14 @@ class Game {
     }
     checkColumns() {
         if ((this.gameBoard[0] === this.gameBoard[3] && this.gameBoard[0] === this.gameBoard[6] && this.gameBoard[6] !== '') || (this.gameBoard[1] === this.gameBoard[4] && this.gameBoard[1] === this.gameBoard[7] && this.gameBoard[7] !== '') || (this.gameBoard[2] === this.gameBoard[5] && this.gameBoard[2] === this.gameBoard[8] && this.gameBoard[8] !== '')) {
+            return true;
+
+        } else {
+            return false;
+        };
+    }
+    checkDiagonals() {
+        if ((this.gameBoard[0] === this.gameBoard[4] && this.gameBoard[0] === this.gameBoard[8] && this.gameBoard[8] !== '') || (this.gameBoard[2] === this.gameBoard[4] && this.gameBoard[2] === this.gameBoard[6] && this.gameBoard[6] !== '')) {
             return true;
 
         } else {
